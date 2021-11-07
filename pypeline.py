9c4351c025b03bdd3bd87a4b4a12d382a075a6a0
Bruno Assis <bruno.assis@live.com>
2021-11-08 00:19:46 +0100

    Enhanced readibility

--- pypeline.py
+++ pypeline.py
@@ -10,55 +10,66 @@ class Record:
     row: int
 
 
-class Middleware:
+class Middleware:    
+    def __init__(self, context):
+        if(context is None):
+            raise TypeError("context can't be None")
+
+        self.pipeline = context
+
     def process(self, record):
         if record is None:
-            logger.debug("No record provided for {self.__name__}")
+            print("No record provided for {self.__class__}")
 
-        print(f"I'm {self.__name__} Processing record {record.name}")
+        print(f"I'm {self.__class__} Processing record {record.name} for {self.pipeline.name}")
 
 
-class MyColMiddleware(Middleware):
-    def __init__(self):
-        pass
-    
+class MultiplyBySevenUnits(Middleware):
+    def __init__(self, context):
+        super().__init__(context)
+            
     def process(self, record: Record):
+        super().process(record)
         record.col = record.col * 7
 
-class MyRowMiddleware(Middleware):
-    def __init__(self):
-        pass
+class SumFiveUnits(Middleware):
+    def __init__(self, context):
+        super().__init__(context)
 
     def process(self, record: Record):
-        record.row = record.col * 5
-
+        super().process(record)
+        record.row = record.row + 5
         
 class Pipeline:
 
-    def __init__(self):
+    def __init__(self, name=__name__):
         self.middlewares = []
+        self.name = name
 
-    def register(self, type: Middleware):
-        self.middlewares.append(type)
+    def register(self, middleware: Middleware):
+        self.middlewares.append(middleware(self))
         return self    
     
     def run(self, record: Record):
         if(record is None):
-            logger.debug("Record can't be None")
-        for i in range(len(self.middlewares)):
-            middleware = self.middlewares[i]
-            middleware.process(middleware, record=record)
-
-        return record   
+            print("Record can't be None")
+        
+        total_middlewares = len(self.middlewares)
+        for index in range(total_middlewares):
+            middleware = self.middlewares[index]
+            middleware.process(record)      
+        
+        return record
 
 if __name__ == "__main__":
     input = Record("Record1", 1, 2)
     
-    pipeline = Pipeline()
-    pipeline.register(MyColMiddleware)
-    pipeline.register(MyRowMiddleware)
+    pipeline = Pipeline("FirstPipeline")
+    pipeline.register(MultiplyBySevenUnits)
+    pipeline.register(SumFiveUnits)
 
-    output = pipeline.run(input)
+    pipeline.run(input)
 
     print(input)
-    print(output)
+    assert input.col == 1*7
+    assert input.row == 2+5
